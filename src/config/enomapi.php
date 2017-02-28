195d7acb513da5c381de5cf156e180506d85e228
Robb Fountain <Robb Fountain>
2017-02-27 21:01:42 -0500

    add config file

---
+++ src/config/enomapi.php
@@ -0,0 +1,22 @@
+<?php
+return [
+	/**
+	 * Your enom reseller username
+	 */
+	'enom_username'	=>	env('ENOM_USER'),
+
+
+	/**
+	 * Your enom reseller password
+	 */
+	'enom_password'	=>	env('ENOM_PASSWORD'),
+
+
+	/**
+	 * The URL for your API calls.  For local development use resellertest
+	 */
+	'api_url'	=>	env('APP_ENV') == 'local' 
+					? 'http://resellertest.enom.com/interface.asp?' 
+					: 'http://enom.com/interface.asp?',
+
+];
