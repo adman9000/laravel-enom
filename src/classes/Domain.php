d90b07e031cfb55204dfa5dc7ad543d69d632d1a
Adam harding <myaddressistaken@googlemail.com>
2018-01-27 16:26:48 +0000

    Additional functions for domain class

--- src/classes/Domain.php
+++ src/classes/Domain.php
@@ -81,4 +81,83 @@ class Domain {
 		$this->api->parseUrl($url);
 		return $this->api->call(['command' => 'GetDomainInfo'])->response();
 	}
+	
+	/**
+	 * Displays status of domain order
+	 * @param  string $url 
+	 * @return array      
+	 */
+	public function getStatus($url) 
+	{
+		$this->api->parseUrl($url);
+		return $this->api->call(['command' => 'GetDomainStatus'])->response();
+	}
+
+	/**
+	 * Displays domain registration price
+	 * @param  string $url 
+	 * @return array      
+	 */
+	public function getPrice($url) 
+	{
+		$this->api->parseUrl($url);
+         $this->api->setParam('ProductType','10');
+		return $this->api->call(['command' => 'PE_GetProductPrice'])->response();
+	}
+
+
+	/**
+	 * Get Domain hosts records (not inc mx records)
+	 * @param  string $url 
+	 * @return array      
+	 */
+	public function getRegHosts($url) {
+
+		$this->api->parseUrl($url);
+		return $this->api->call(['command' => 'GetRegHosts'])->response();
+
+	}
+
+	/**
+	 * Set Domain hosts records
+	 * @param  string $url 
+	 * @param  array $hosts 
+	 * @return array      
+	 */
+	public function setHosts($url, $hosts) {
+
+		$this->api->parseUrl($url);
+
+		$i = 0;
+		foreach($hosts as $host) {
+			$i++;
+			$this->api->setParam("HostName".$i, $host['HostName']);
+			$this->api->setParam("RecordType".$i, $host['RecordType']);
+			$this->api->setParam("Address".$i, $host['Address']);
+			if(isset($host['MXPref'])) $this->api->setParam("MXPref".$i, $host['MXPref']);
+		}
+
+		return $this->api->call(['command' => 'SetHosts'])->response();
+
+	}
+
+	/**
+	 * Set nameservers
+	 * @param  string $url 
+	 * @param  array $nameservers 
+	 * @return array      
+	 */
+	public function ModifyNS($url, $nameservers) {
+		
+		$this->api->parseUrl($url);
+
+		$i = 0;
+		foreach($nameservers as $nameserver) {
+			$i++;
+			$this->api->setParam("NS".$i, $nameserver);
+		}
+
+		return $this->api->call(['command' => 'ModifyNS'])->response();
+
+	}
 }
